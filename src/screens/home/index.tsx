6f7a299d1d1461ec229ee400b35abb91d07663a1
larrikin-coder <shauryat.22703@gmail.com>
2024-10-07 20:40:51 +0530

    Drawing canvas started

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,28 @@
+import { useRef, useState } from "react";
+
+export default function Home(){
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) =>{
+        const canvas = canvasRef.current;
+        if (canvas){
+            canvas.style.background = "black";
+            const ctx = canvas.getContext('2d');
+            if (ctx){
+                ctx.beginPath();
+                ctx.moveTo(e.nativeEvent.offsetX,e.nativeEvent.offsetY);
+                setIsDrawing(true);
+            }
+        }
+    }
+    return(
+        <>
+        <canvas
+            ref = {canvasRef}
+            id = 'canvas'
+            className = 'absolute top-0 left-0 w-full h-full'
+            onMouseDown={startDrawing}
+        />
+        </>
+)
+}
